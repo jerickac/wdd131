206b49f10a40e41081125f76700924ada75f98a3
jerickac <joaoerickacr@gmail.com>
2024-12-19 16:08:13 -0300

    W5 assignment partially done

---
+++ week05/Learning Activities/Local Storage - Enhancing BOM application/scripts/bom.js
@@ -0,0 +1,36 @@
+const ipt = document.querySelector("#favchap");
+const btn = document.querySelector("button");
+const list = document.querySelector("#list");   
+
+chaptersArray = getChapterList() || [];
+
+btn.addEventListener('click', () => {
+    const favoriteScript = ipt.value;
+    ipt.value = "";
+
+    if (favoriteScript.trim() !== '') {
+    
+        const scriptInList = document.createElement("li");
+        const span = document.createElement("span");
+        const deleteBtn = document.createElement("button");        
+    
+        scriptInList.appendChild(span);
+        span.textContent = favoriteScript;
+
+        scriptInList.appendChild(deleteBtn);
+        deleteBtn.textContent = "❌";
+
+        list.appendChild(scriptInList);
+
+        deleteBtn.addEventListener('click', () => {
+            list.removeChild(scriptInList);
+        })
+    } else {
+        alert("Please type a scripture reference");
+    }
+
+
+    ipt.focus();
+    // scriptInList.appendChild(favoriteScript);
+
+});
